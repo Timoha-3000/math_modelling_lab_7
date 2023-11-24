599647f3ccc5d92dca2599065af018bbac5240d5
pl_konstkri <fcbsport2015@bk.ru>
2023-11-25 01:15:17 +0300

    матрица выглядит теперь куда более читабельно

--- Program.cs
+++ Program.cs
@@ -10,26 +10,8 @@ class Program
         Console.WriteLine("Матрица взаимодолгов:");
         DisplayMatrix(debts);
 
-        Console.WriteLine("\nСначала рассчитываем сальдо:");
-        int[] initialBalances = CalculateBalance(debts);
-
-        Console.WriteLine("\nПроводим взаимоотчет:");
-        int[] finalBalances = ConductSettlement(debts, initialBalances);
-
-        Console.WriteLine("\nСальдо до взаимоотчета:");
-        DisplayBalances(initialBalances);
-
-        Console.WriteLine("\nСальдо после взаимоотчета:");
-        DisplayBalances(finalBalances);
-
-        if (CheckBalancesEquality(initialBalances, finalBalances))
-        {
-            Console.WriteLine("\nСальдо совпадают.");
-        }
-        else
-        {
-            Console.WriteLine("\nСальдо не совпадают. Программа не может выполнить взаиморасчеты.");
-        }
+        Console.WriteLine("\nВзаимоотчет:");
+        ConductSettlement(debts);
     }
 
     // Генерация случайных долгов между предприятиями
@@ -58,8 +40,19 @@ class Program
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
 
+        Console.Write("   "); // Пустая ячейка для отступа
+
+        // Вывод заголовков столбцов (A, B, C...)
+        for (int i = 0; i < cols; i++)
+        {
+            Console.Write($"{(char)('A' + i),4}"); // Заголовки столбцов
+        }
+        Console.WriteLine();
+
         for (int i = 0; i < rows; i++)
         {
+            Console.Write($"{(char)('A' + i),2} "); // Заголовки строк
+
             for (int j = 0; j < cols; j++)
             {
                 Console.Write(matrix[i, j].ToString().PadLeft(4)); // Выравниваем числа
@@ -68,40 +61,10 @@ class Program
         }
     }
 
-    // Расчет сальдо предприятий
-    static int[] CalculateBalance(int[,] debts)
+    // Проведение взаимоотчета
+    static void ConductSettlement(int[,] debts)
     {
         int numCompanies = debts.GetLength(0);
-        int[] balances = new int[numCompanies];
-
-        for (int i = 0; i < numCompanies; i++)
-        {
-            int totalDebtToOthers = 0;
-            int totalDebtFromOthers = 0;
-
-            for (int j = 0; j < numCompanies; j++)
-            {
-                if (i != j)
-                {
-                    totalDebtToOthers += debts[i, j];
-                    totalDebtFromOthers += debts[j, i];
-                }
-            }
-
-            balances[i] = totalDebtFromOthers - totalDebtToOthers;
-
-            Console.WriteLine($"Сальдо для предприятия {(char)('A' + i)}: {balances[i]}");
-        }
-
-        return balances;
-    }
-
-    // Проведение взаимоотчета с коррекцией сальдо
-    static int[] ConductSettlement(int[,] debts, int[] balances)
-    {
-        int numCompanies = debts.GetLength(0);
-        int[] updatedBalances = new int[numCompanies];
-        Array.Copy(balances, updatedBalances, numCompanies);
 
         for (int i = 0; i < numCompanies; i++)
         {
@@ -109,41 +72,12 @@ class Program
             {
                 if (i != j && debts[i, j] > 0)
                 {
-                    if (updatedBalances[i] > 0 && updatedBalances[j] < 0)
-                    {
-                        int payment = Math.Min(debts[i, j], Math.Abs(updatedBalances[j]));
-                        debts[i, j] -= payment;
-                        updatedBalances[i] -= payment;
-                        updatedBalances[j] += payment;
-                    }
+                    Console.WriteLine($"Предприятие {(char)('A' + i)} погашает долг {debts[i, j]} предприятию {(char)('A' + j)}");
+                    debts[i, j] = 0; // Погашаем долг
                 }
             }
         }
 
-        return updatedBalances;
-    }
-
-    // Вывод сальдо в консоль
-    static void DisplayBalances(int[] balances)
-    {
-        int numCompanies = balances.Length;
-
-        for (int i = 0; i < numCompanies; i++)
-        {
-            Console.WriteLine($"Сальдо для предприятия {(char)('A' + i)}: {balances[i]}");
-        }
-    }
-
-    // Проверка совпадения сальдо до и после взаимоотчета
-    static bool CheckBalancesEquality(int[] initialBalances, int[] finalBalances)
-    {
-        for (int i = 0; i < initialBalances.Length; i++)
-        {
-            if (initialBalances[i] != finalBalances[i])
-            {
-                return false;
-            }
-        }
-        return true;
+        Console.WriteLine("\nВзаимоотчет завершен.");
     }
 }
